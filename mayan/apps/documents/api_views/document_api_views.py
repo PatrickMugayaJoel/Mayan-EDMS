3dc7baf9952a88b0f6019866bbb164d332da85d9
Mugaya Joel Patrick <patrickthemajesty@gmail.com>
2023-08-31 12:12:08 +0300

    Update reminders query to pick workflow name

--- mayan/apps/documents/api_views/document_api_views.py
+++ mayan/apps/documents/api_views/document_api_views.py
@@ -134,41 +134,44 @@ class APISendMailReminders(APIView):
 
     def get(self, request, *args, **kwargs):
 
-        pending_stages = Document.objects.raw(f"""SELECT nested.workflow as wx, nested.state_id as state_id, nested.state_id as id, count(nested.state_id) as ecount, nts.email
+        pending_stages = Document.objects.raw(f"""SELECT nested.workflow as wx, nested.label, nested.state_id as state_id, nested.state_id as id, count(nested.state_id) as ecount, nts.email
             FROM (
-                SELECT DISTINCT ON(id) swi.id AS id, swi.workflow_id AS workflow, sws.id AS state_id, max(swil.datetime) AS datetime
+                SELECT DISTINCT ON(id) swi.id AS id, swi.workflow_id AS workflow, wf.label, sws.id AS state_id, max(swil.datetime) AS datetime
                 FROM document_states_workflowinstance swi, document_states_workflowinstancelogentry swil,
-                document_states_workflowtransition swt, document_states_workflowstate sws
+                document_states_workflowtransition swt, document_states_workflowstate sws, document_states_workflow wf
                 WHERE swi.workflow_id in (SELECT * FROM public."nic_tracked_workflows")
                     AND swil.workflow_instance_id = swi.id
                     AND swil.transition_id = swt.id
                     AND swt.destination_state_id = sws.id
-                group by swi.id, sws.id
+                    AND wf.id = swi.workflow_id
+                group by swi.id, sws.id, wf.id
                 order by id, datetime desc
             ) AS nested, nic_tracked_states nts
             WHERE nested.state_id in (SELECT state_id FROM public."nic_tracked_states")
             AND nested.state_id = nts.state_id
             AND nested.datetime < (now() - '20 hours'::interval)
-            group by nts.email, nested.state_id, nested.workflow
+            group by nts.email, nested.state_id, nested.workflow, nested.label
 
             UNION
 
-            SELECT max(9) AS wx, max(66) AS state_id, max(swi.document_id) AS id, count(md.value) as ecount, md.value AS email
-            FROM document_states_workflowinstance swi, document_states_workflowinstancelogentry swil, metadata_documentmetadata md
+            SELECT max(9) AS wx, wf.label, max(66) AS state_id, max(swi.document_id) AS id, count(md.value) as ecount, md.value AS email
+            FROM document_states_workflowinstance swi, document_states_workflowinstancelogentry swil, metadata_documentmetadata md, document_states_workflow wf
             WHERE swi.workflow_id = 9
             AND md.metadata_type_id = 16
             AND swil.transition_id = 308
             AND md.document_id = swi.document_id
             AND swil.workflow_instance_id = swi.id
             AND swi.document_id NOT IN (
-            select wi.document_id
-            FROM document_states_workflowinstance wi
-            join document_states_workflowinstancelogentry wg
-            on wg.workflow_instance_id = wi.id
-            WHERE wi.workflow_id = 9
-            AND wg.transition_id in (187, 302, 189, 307, 85, 87, 88, 89, 178))
+                select wi.document_id
+                FROM document_states_workflowinstance wi
+                join document_states_workflowinstancelogentry wg
+                on wg.workflow_instance_id = wi.id
+                WHERE wi.workflow_id = 9
+                AND wg.transition_id in (187, 302, 189, 307, 85, 87, 88, 89, 178)
+            )
             AND swil.datetime < (now() - '20 hours'::interval)
-            group by md.value;"""
+            AND wf.id = swi.workflow_id
+            group by md.value, wf.id;"""
         )
 
         today = date.today()
@@ -183,14 +186,7 @@ class APISendMailReminders(APIView):
 
         for stage in pending_stages:
 
-            workflow = 'Edms'
-            if stage.wx in [1, 6]:
-                workflow = 'Claim'
-            elif stage.wx == 13:
-                workflow = 'Medical'
-            elif stage.wx == 9:
-                workflow = 'Leave'
-
+            workflow = stage.label.split()[0]
             grammer = []
             if stage.ecount > 1:
                 grammer.append("files have")
