5d17ce4b161aca66d283986533b79ba06365d75c
Mugaya Joel <43676847+MugayaJoelPatrick@users.noreply.github.com>
2023-10-25 10:52:28 +0300

    Editing PROJECT_URL that shows in password reset

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,4 +1,6 @@
-__title__ = 'Mayan EDMS'
+import os
+
+__title__ = 'EDMS'
 __version__ = '4.0.16'
 __build__ = 0x040016
 __build_string__ = 'v4.0.16_Wed Sep 29 03:10:13 2021 -0400'
@@ -9,4 +11,4 @@ __description__ = 'Free Open Source Electronic Document Management System'
 __license__ = 'Apache 2.0'
 __copyright_short__ = '2011 Roberto Rosario'
 __copyright__ = '{} {}'.format('Copyright', __copyright_short__)
-__website__ = 'https://www.mayan-edms.com'
+__website__ = os.environ.get('PROJECT_URL') # 'https://www.mayan-edms.com'
